e118a761f3efb6edd89e385c9a5d79a5383783b0
fuchssohn <glademir.karpinski.jr@gmail.com>
2020-09-11 09:42:25 -0300

    bug on a csv file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     # Mandatory and initial setup
     name='vbelts',
-    version='0.1.0',
+    version='0.1.1',
     description='Utilities for v-belt dimensioning',
     long_description=long_description,
     long_description_content_type="text/markdown",
